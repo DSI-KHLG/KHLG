e1e3bb29f83a9db910486a030d8c4f785e4ce245
Rico <ricolittawa030620@gmail.com>
2026-03-10 14:15:40 +0800

    added css and js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Scroll reveal animation
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll(){
+
+reveals.forEach(element => {
+
+const windowHeight = window.innerHeight;
+const revealTop = element.getBoundingClientRect().top;
+const revealPoint = 100;
+
+if(revealTop < windowHeight - revealPoint){
+element.classList.add("active");
+}
+
+});
+
+}
+
+window.addEventListener("scroll", revealOnScroll);
